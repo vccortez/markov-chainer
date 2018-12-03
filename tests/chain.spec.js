ebf393e18f694577f7bcd0ee87f0c0b62d4ff2ff
Vitor Cortez <vitor.a.cortez@gmail.com>
2018-12-03 11:55:55 -0300

    Add initial tests

---
+++ tests/chain.spec.js
@@ -0,0 +1,27 @@
+const test = require('tape')
+
+const { Chain } = require('../lib')
+
+const corpus = [
+  ['Hello', 'world', 'of', 'Markov', 'chains'],
+  ['These', 'are', 'my', "process'", 'tokens'],
+  ['This', 'can', 'be', 'any', 'JSON', 'data'],
+  ['I', 'can', 'use', 'other', { a: 'types' }]
+]
+
+test('chain serialisation', (t) => {
+  t.plan(4)
+
+  const options = { corpus, order: 1, useTokenMap: true }
+
+  const original = new Chain(options)
+  const serialised = JSON.stringify(original)
+
+  t.ok(typeof serialised === 'string', 'chain should be serialisable by `JSON.strinfigy`')
+
+  const generated = Chain.fromJSON(serialised, options)
+
+  t.equal(generated.order, original.order, 'generated chain should have the same order as original')
+  t.same(generated.tokenMap, original.tokenMap, 'generated chain should have the same token map as original')
+  t.same(generated.model, original.model, 'generated chain model should be the same as the original')
+})
